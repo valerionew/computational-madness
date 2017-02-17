4b0b889d114fcadb13365f1676159b5dd0a070c0
Valerio Nappi <valerionappi995@gmail.com>
2017-02-17 02:32:24 +0100

    Create easiest-pi.c
    This is the easiest way we can make a montecarlo algorithm calculating pi

---
+++ easiest-pi.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define POINTS 1000000000  //the number of random points to be generated
+
+int main()
+{
+  srand(time(NULL)); //setting the random seed
+  double x,y; //the generated point coordinates
+
+  unsigned long int belongs=0,executed=0;
+
+
+  for(executed=0;executed<POINTS;executed++)
+  {
+    //generate the point
+    x=rand()/(double)RAND_MAX;
+    y=rand()/(double)RAND_MAX;
+    //check if the generated point belongs to the circle or not
+    if(x*x+y*y<=1)
+    {
+      belongs++;
+    }
+  }
+
+  //print my result out
+  printf("%lf\n", (4.0*belongs)/executed);
+
+  return 0;
+}
